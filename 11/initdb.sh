70aabde94129c9d7b12da7ff0ce832cb4dd4a64c
Andrew N. Shalaev <isqad88@gmail.com>
2019-12-05 13:39:55 +0500

    feature: add RUM extension

--- 11/initdb.sh
+++ 11/initdb.sh
@@ -4,12 +4,14 @@ psql -v ON_ERROR_STOP=1 --username postgres template1 <<-EOSQL
 CREATE EXTENSION hstore;
 CREATE EXTENSION pg_trgm;
 CREATE EXTENSION intarray;
+CREATE EXTENSION rum;
 EOSQL
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 CREATE EXTENSION hstore;
 CREATE EXTENSION pg_trgm;
 CREATE EXTENSION intarray;
+CREATE EXTENSION rum;
 EOSQL
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
